bba9fa699a966c75a37b1f558d1cb046f4b1d1b6
CarrozC <116290344+CarrozC@users.noreply.github.com>
2023-07-28 16:39:12 -0400

    Update lab9.c

--- lab9.c
+++ lab9.c
@@ -8,16 +8,22 @@ struct RecordType
 	int		order; 
 };
 
-// Fill out this structure
+struct Node
+{
+	struct RecordType data;
+	struct Node* next;
+};
+
 struct HashType
 {
-
+	struct Node** hashArray;
+	int size;
 };
 
 // Compute the hash function
-int hash(int x)
+int hash(int x, int tableSize)
 {
-
+	return x % tableSize;
 }
 
 // parses input file to an integer array
@@ -73,22 +79,84 @@ void printRecords(struct RecordType pData[], int dataSz)
 // skip the indices which are free
 // the output will be in the format:
 // index x -> id, name, order -> id, name, order ....
-void displayRecordsInHash(struct HashType *pHashArray, int hashSz)
-{
-	int i;
 
-	for (i=0;i<hashSz;++i)
+void insertRecord(struct HashType* pHashArray, int id, char name, int order)
+{
+	int hashValue = hash(id, pHashArray->size);
+
+	struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+	if (newNode == NULL)
 	{
-		// if index is occupied with any records, print all
+		printf("Memory allocation failed for a new node.\n");
+		exit(-1);
 	}
+
+	newNode->data.id = id;
+	newNode->data.name = name;
+	newNode->data.order = order;
+	newNode->next = NULL;
+
+	newNode->next = pHashArray->hashArray[hashValue];
+	pHashArray->hashArray[hashValue] = newNode;
 }
+
+void displayRecordsInHash(struct HashType* pHashArray)
+{
+	printf("\nRecords in the hash table:\n");
+	for (int i = 0; i < pHashArray->size; ++i)
+	{
+		printf("Index %d -> ", i);
+		struct Node* currentNode = pHashArray->hashArray[i];
+		while (currentNode != NULL)
+		{
+			printf("%d %c %d -> ", currentNode->data.id, currentNode->data.name, currentNode->data.order);
+			currentNode = currentNode->next;
+		}
+		printf("NULL\n");
+	}
+	printf("\n");
+}
+
 
 int main(void)
 {
-	struct RecordType *pRecords;
+	struct RecordType* pRecords;
 	int recordSz = 0;
 
 	recordSz = parseData("input.txt", &pRecords);
 	printRecords(pRecords, recordSz);
-	// Your hash implementation
+
+	int hashSize = recordSz * 2;
+	struct HashType hashTable;
+	hashTable.size = hashSize;
+	hashTable.hashArray = (struct Node**)malloc(sizeof(struct Node*) * hashSize);
+	if (hashTable.hashArray == NULL)
+	{
+		printf("Memory allocation failed for the hash table.\n");
+		exit(-1);
+	}
+	for (int i = 0; i < hashSize; ++i)
+		hashTable.hashArray[i] = NULL;
+
+	for (int i = 0; i < recordSz; ++i)
+	{
+		insertRecord(&hashTable, pRecords[i].id, pRecords[i].name, pRecords[i].order);
+	}
+
+	displayRecordsInHash(&hashTable);
+
+	for (int i = 0; i < hashSize; ++i)
+	{
+		struct Node* currentNode = hashTable.hashArray[i];
+		while (currentNode != NULL)
+		{
+			struct Node* temp = currentNode;
+			currentNode = currentNode->next;
+			free(temp);
+		}
+	}
+	free(hashTable.hashArray);
+	free(pRecords);
+
+	return 0;
 }
